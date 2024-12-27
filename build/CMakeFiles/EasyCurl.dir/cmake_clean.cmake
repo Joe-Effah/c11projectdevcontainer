e363649f1a687a2342c6df94e4c55ea5ce782f77
Obed Danso <obed.danso@scynett.com>
2024-12-27 08:59:38 +0000

    more add a strurtuce

---
+++ build/CMakeFiles/EasyCurl.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EasyCurl.dir/src/main.c.obj"
+  "CMakeFiles/EasyCurl.dir/src/main.c.obj.d"
+  "EasyCurl.exe"
+  "EasyCurl.exe.manifest"
+  "EasyCurl.pdb"
+  "libEasyCurl.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/EasyCurl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

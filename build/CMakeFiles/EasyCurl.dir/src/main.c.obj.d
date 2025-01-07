af49b78f851fa686482b0d55ace38c26d13dd7a9
Obed Danso <obed.danso@scynett.com>
2025-01-07 07:14:49 +0000

    removed the build folder

--- build/CMakeFiles/EasyCurl.dir/src/main.c.obj.d
+++
@@ -1,9 +0,0 @@
-CMakeFiles/EasyCurl.dir/src/main.c.obj: \
- D:\Programming\Personal\ Projects\C\ Projects\Projects\EasyCurl\src\main.c \
- d:\libraries\mingw\include\stdio.h d:\libraries\mingw\include\_mingw.h \
- d:\libraries\mingw\include\msvcrtver.h \
- d:\libraries\mingw\include\w32api.h \
- d:\libraries\mingw\include\sdkddkver.h \
- d:\libraries\mingw\lib\gcc\mingw32\6.3.0\include\stddef.h \
- d:\libraries\mingw\include\sys/types.h \
- d:\libraries\mingw\lib\gcc\mingw32\6.3.0\include\stdarg.h
